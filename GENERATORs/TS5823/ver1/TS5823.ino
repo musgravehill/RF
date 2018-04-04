c7fbe305a4a9b458d0bfbc182719797b91cedddd
Bob_work <musgravehill@gmail.com>
2018-04-04 21:24:48 +0300

    ts

--- GENERATORs/TS5823/ver1/TS5823.ino
+++ GENERATORs/TS5823/ver1/TS5823.ino
@@ -22,3 +22,45 @@ void TS5823_set_FR4() { //F
   digitalWrite(TS5823_pin_4, HIGH); //OFF switch
   digitalWrite(TS5823_pin_5, HIGH); //OFF switch
 }
+
+void TS5823_set_CH1() {
+  digitalWrite(TS5823_pin_1, LOW); //ON switch
+  digitalWrite(TS5823_pin_2, LOW); //ON switch
+  digitalWrite(TS5823_pin_3, LOW); //ON switch
+}
+void TS5823_set_CH2() {
+  digitalWrite(TS5823_pin_1, HIGH); //OFF switch
+  digitalWrite(TS5823_pin_2, LOW); //ON switch
+  digitalWrite(TS5823_pin_3, LOW); //ON switch
+}
+void TS5823_set_CH3() {
+  digitalWrite(TS5823_pin_1, LOW); //ON switch
+  digitalWrite(TS5823_pin_2, HIGH); //OFF switch
+  digitalWrite(TS5823_pin_3, LOW); //ON switch
+}
+void TS5823_set_CH4() {
+  digitalWrite(TS5823_pin_1, HIGH); //OFF switch
+  digitalWrite(TS5823_pin_2, HIGH); //OFF switch
+  digitalWrite(TS5823_pin_3, LOW); //ON switch
+}
+void TS5823_set_CH5() {
+  digitalWrite(TS5823_pin_1, LOW); //ON switch
+  digitalWrite(TS5823_pin_2, LOW); //ON switch
+  digitalWrite(TS5823_pin_3, HIGH); //OFF switch
+}
+void TS5823_set_CH6() {
+  digitalWrite(TS5823_pin_1, HIGH); //OFF switch
+  digitalWrite(TS5823_pin_2, LOW); //ON switch
+  digitalWrite(TS5823_pin_3, HIGH); //OFF switch
+}
+void TS5823_set_CH7() {
+  digitalWrite(TS5823_pin_1, LOW); //ON switch
+  digitalWrite(TS5823_pin_2, HIGH); //OFF switch
+  digitalWrite(TS5823_pin_3, HIGH); //OFF switch
+}
+void TS5823_set_CH8() {
+  digitalWrite(TS5823_pin_1, HIGH); //OFF switch
+  digitalWrite(TS5823_pin_2, HIGH); //OFF switch
+  digitalWrite(TS5823_pin_3, HIGH); //OFF switch
+}
+

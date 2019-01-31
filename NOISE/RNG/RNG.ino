9ff0f1145e6087875edaa09cba8f5193e0ab708e
Bob_work <musgravehill@gmail.com>
2019-02-01 00:59:41 +0300

    rng

--- NOISE/RNG/RNG.ino
+++ NOISE/RNG/RNG.ino
@@ -2,8 +2,8 @@ const int analogInPin = A0;
 int sensorValue = 0;
 
 
-//uint32_t sum = 0L;
-//uint32_t i = 0L;
+uint32_t sum = 0L;
+uint32_t i = 0L;
 uint32_t c_0 = 0L;
 uint32_t c_1 = 0L;
 
@@ -14,22 +14,24 @@ void setup() {
 void loop() {
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
+  Serial.print(sensorValue, BIN);
+  /*
+    sum += sensorValue;
+    i++;
+    uint16_t aver = sum / i;
 
-  //sum += sensorValue;
-  //i++;
-
-  if (sensorValue > 446) {
+    if (sensorValue > aver) {
     c_1++;
-  } else {
+    } else {
     c_0++;
-  }
+    }
 
-  //Serial.print(sensorValue, BIN);
-  delay(2);
-  if ((millis() % 5000) < 2) {
+    //Serial.print(sensorValue, BIN);
+    delay(2);
+    if ((millis() % 5000) < 2) {
     //Serial.println(sum / i);
     Serial.print(c_0);
     Serial.print(' ');
     Serial.println(c_1);
-  }
+    }*/
 }

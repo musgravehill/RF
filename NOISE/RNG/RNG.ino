60c79cfb5684505fdce889b96946ea893bc23a99
Bob_work <musgravehill@gmail.com>
2019-02-01 00:45:10 +0300

    ardu

--- NOISE/RNG/RNG.ino
+++ NOISE/RNG/RNG.ino
@@ -1,8 +1,9 @@
 const int analogInPin = A0;
 int sensorValue = 0;
 
-uint32_t count_0 = 0L;
-uint32_t count_1 = 0L;
+
+uint32_t sum = 0L;
+uint32_t i = 0L;
 
 void setup() {
   Serial.begin(9600);
@@ -12,22 +13,12 @@ void loop() {
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
 
-  byte out[8];
-  byte adc = (sensorValue >> 3) + 128;
-  for (byte i = 0; i < 8; i++) {
-    out[i] = adc & (1 << (7 - i)) ? 1 : 0;
-    if (out[i] == 1) {
-      count_1++;
-    } else {
-      count_0++;
-    }
-  }
+  sum += sensorValue;
+  i++;
 
   //Serial.print(sensorValue, BIN);
   delay(2);
-  if (millis() % 2000) {
-    Serial.print(count_0);
-    Serial.print(' ');
-    Serial.println(count_1);
+  if ((millis() % 5000) < 2) {
+    Serial.println(sum / i);
   }
 }

9e64b239a2eb1ea04f71043305c67eba747c1a50
Bob_work <musgravehill@gmail.com>
2019-02-01 11:47:33 +0300

    ent

--- NOISE/RNG/RNG.ino
+++ NOISE/RNG/RNG.ino
@@ -1,35 +1,66 @@
 const int analogInPin = A0;
-int sensorValue = 0;
+uint16_t sensorValue = 0;
+uint16_t sensorValuePrev = 0;
 
-
-uint32_t sum = 0L;
-uint32_t i = 0L;
+/*
+  #define average_count 128
+  uint16_t average_val = 1;
+  uint8_t average_i = 0;
+  uint16_t average_el[average_count];
+*/
 uint32_t c_0 = 0L;
 uint32_t c_1 = 0L;
+uint8_t eol_i = 0;
 
 void setup() {
   Serial.begin(9600);
+  /*for (int i = 0; i < average_count; i++) {
+    average_el[i] = 0;
+    }*/
 }
 
 void loop() {
-  // read the analog in value:
+  delay(2); //TODO test delays 0..***
+  sensorValuePrev = sensorValue;
   sensorValue = analogRead(analogInPin);
-  Serial.print(sensorValue, BIN);
-  /*
-    sum += sensorValue;
-    i++;
-    uint16_t aver = sum / i;
 
-    if (sensorValue > aver) {
+  //Serial.println();
+  //Serial.print("curr=");
+  //Serial.println(sensorValue, DEC);
+
+  /*
+
+    average_el[average_i] = sensorValue;
+    average_i++;
+    if (average_i >= average_count) {
+    average_i = 0;
+
+    uint32_t average_sum = 0L;
+    for (uint8_t i = 0; i < average_count; i++) {
+      average_sum += average_el[i];
+    }
+    average_val = 0 * average_val + 1 * average_sum / average_count;
+    }
+
+    if (sensorValue > average_val) {
     c_1++;
     } else {
     c_0++;
     }
+  */
 
-    //Serial.print(sensorValue, BIN);
-    delay(2);
-    if ((millis() % 5000) < 2) {
-    //Serial.println(sum / i);
+  if (sensorValuePrev > sensorValue) {
+    //c_1++;
+    Serial.print(1);
+  } else {
+    //c_0++;
+    Serial.print(0);
+  } 
+
+
+  /*if ((millis() % 5000) < 2) {
+    //Serial.print(average_val);
+    //Serial.print(' ');
     Serial.print(c_0);
     Serial.print(' ');
     Serial.println(c_1);
